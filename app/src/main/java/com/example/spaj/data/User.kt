2c69bdbcd70c2edf625498d2f8d2b78bb1dcda59
davinkaghaly <davinkaghaly12345@gmail.com>
2025-08-01 08:29:49 +0700

    asdlkasdlkajawd

---
+++ app/src/main/java/com/example/spaj/data/User.kt
@@ -0,0 +1,7 @@
+package com.example.spaj.data
+
+data class User(
+    val id: Int,
+    val nama: String,
+    val role: String // contoh: "siswa", "guru", "kepala_jurusan"
+)
---
+++ app/src/main/java/com/example/spaj/data/barang.kt
@@ -0,0 +1,8 @@
+package com.example.spaj.data
+
+data class Barang(
+    val id: Int,
+    val nama: String,
+    val jurusan: String,
+    val tersedia: Boolean
+)
